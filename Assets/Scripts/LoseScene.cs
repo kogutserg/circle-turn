e6f7e4a99c7e345648959d7a219a9a5e5883eb82
Sergii Kogut <skogut@playtika.com>
2021-10-27 21:27:12 +0300

    Added aclculate score

--- Assets/Scripts/LoseScene.cs
+++ Assets/Scripts/LoseScene.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoseScene : MonoBehaviour
 {
+    public int score;
+    public Text scoreText;
+
+    public void Start()
+    {
+        score = PlayerPrefs.GetInt("Score");
+    }
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
     public void ToMenu()
     {
         SceneManager.LoadScene(0);
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public int money;
+    public int earnedMoney;
+    public Text moneyText;
+
+    public void Start()
+    {
+        money = PlayerPrefs.GetInt("Money");
+        earnedMoney = PlayerPrefs.GetInt("Score");
+        money += earnedMoney;
+        PlayerPrefs.SetInt("Money", money);
+        moneyText.text = money.ToString();
+        earnedMoney = 0;
+        PlayerPrefs.SetInt("Score", earnedMoney);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
